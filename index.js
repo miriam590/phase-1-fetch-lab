408595e4e83b09ddf978a1e3ce9695e3b76bfba6
miria-ce <miriam.yego@student.moringashool.com>
2024-12-13 12:44:35 +0100

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  const url ="https://anapioficeandfire.com/api/books"
+  return fetch(url)
+  .then(response => response.json())
+  .then(json => renderBooks(json))
+
   
 }
 
